9e07f3f86d7d1fe714f769f08d22c9077f16277f
Djordje <djordjemilic33@gmail.com>
2023-09-22 22:56:28 +0200

    Inital commit and create navbar component

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  firebase: {
+    projectId: 'password-manager-9191c',
+    appId: '1:809173297916:web:944ade7a2b19bfbde3c579',
+    storageBucket: 'password-manager-9191c.appspot.com',
+    apiKey: 'AIzaSyCPyOMRM1WpyJ4WTV6EiBIMCv27QdwyjyE',
+    authDomain: 'password-manager-9191c.firebaseapp.com',
+    messagingSenderId: '809173297916',
+    measurementId: 'G-GVP06K8FHM',
+  },
+  production: false
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  firebase: {
+    projectId: 'password-manager-9191c',
+    appId: '1:809173297916:web:944ade7a2b19bfbde3c579',
+    storageBucket: 'password-manager-9191c.appspot.com',
+    apiKey: 'AIzaSyCPyOMRM1WpyJ4WTV6EiBIMCv27QdwyjyE',
+    authDomain: 'password-manager-9191c.firebaseapp.com',
+    messagingSenderId: '809173297916',
+    measurementId: 'G-GVP06K8FHM',
+  },
+  production: true
 };
 
 /*
